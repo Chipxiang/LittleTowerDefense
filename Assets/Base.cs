b611c1088ee9a0b7bd815679c63afc1ac9e85701
fbybeetle <461971750@qq.com>
2017-11-05 00:31:17 -0500

    add base

---
+++ Assets/Base.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var onCell = FindObjectsOfType<Cell>()[0];
+        this.transform.position = new Vector3(onCell.transform.position.x, onCell.transform.position.y, onCell.transform.position.z + 0.5f);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

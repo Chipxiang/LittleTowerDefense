53655e4d2937e7b1112395cff5c91cd063a6115c
Chipxiang <jianxiangli1996@gmail.com>
2017-11-05 01:10:53 -0500

    Update

--- Assets/Base.cs
+++ Assets/Base.cs
@@ -6,10 +6,8 @@ public class Base : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        var onCell = FindObjectsOfType<Cell>()[0];
-        this.transform.position = new Vector3(onCell.transform.position.x, onCell.transform.position.y, onCell.transform.position.z + 0.5f);
-		
-	}
+        this.transform.position = new Vector3(4f, 0f, - 0.5f);
+    }
 	
 	// Update is called once per frame
 	void Update () {

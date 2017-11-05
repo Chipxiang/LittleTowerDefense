6f77c4d3976198ca298f7d5e89319dde1f7817c7
Chipxiang <jianxiangli1996@gmail.com>
2017-11-05 01:53:33 -0500

    upload

--- Assets/Base.cs
+++ Assets/Base.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Base : MonoBehaviour {
-
+    private float baseHealth;
 	// Use this for initialization
 	void Start () {
         this.transform.position = new Vector3(4f, 0f, - 0.5f);
+        baseHealth = 100;
     }
-	
-	// Update is called once per frame
-	void Update () {
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.GetComponent<Enemy>())
+        {
+            SliderManager.DeductHealth(5f);
+        }
+    }
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
---
+++ Assets/SliderManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderManager : MonoBehaviour {
+    private static float health;
+    private static Slider slider;
+    // Use this for initialization
+    internal void Start () {
+		slider = GetComponent<Slider>();
+        UpdateSlider();
+    }
+    public static void DeductHealth(float points)
+    {
+        health -= points;
+        if (health < 0)
+            health = 0;
+        UpdateSlider();
+    }
+    public static void UpdateSlider () {
+        slider.value = health;
+	}
+}

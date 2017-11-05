15cf49ba2eef0aab3ff6fd6828b38d2b89cf857e
Chipxiang <jianxiangli1996@gmail.com>
2017-11-05 01:18:35 -0600

    update

--- Assets/Base.cs
+++ Assets/Base.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Base : MonoBehaviour {
+public class Base : MonoBehaviour
+{
     private float baseHealth;
-	// Use this for initialization
-	void Start () {
-        this.transform.position = new Vector3(4f, 0f, - 0.5f);
+    // Use this for initialization
+    void Start()
+    {
+        this.transform.position = new Vector3(4f, 0f, -1.5f);
         baseHealth = 100;
     }
     private void OnCollisionEnter(Collision collision)
@@ -16,15 +18,4 @@ public class Base : MonoBehaviour {
             SliderManager.DeductHealth(5f);
         }
     }
-<<<<<<< HEAD
-    // Update is called once per frame
-    void Update () {
-		
-	}
-=======
-        // Update is called once per frame
-        void Update () {
-
-    }
->>>>>>> 096f3840b56728d47b7456daa6ae4343884777cd
 }

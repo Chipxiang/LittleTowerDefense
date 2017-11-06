114bad2f60c28dd435724866acfbfebb8e8b9370
lennyKim <heliotherapyy@gmail.com>
2017-11-05 19:37:27 -0600

    Merge complete with camera and some fix ups from tower, enemy, bullet

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-	public float Health;
-	public float Damage;
-	public float Speed;
+  public float Health;
+  public float Damage;
+  public float Speed;
     Transform Target;
     // public static EnemySpawner Manager;
     void Start()
@@ -14,35 +14,39 @@ public class Enemy : MonoBehaviour
         Target = FindObjectOfType<Base>().transform;
     }
     public void Initialize ()
-	{
+  {
         transform.position = new Vector3(0, 4, -0.7f);
-		Health = 25f;
-		Damage = 5f;
-		Speed = 0.5f;
-	}
-	
-	 internal void OnCollisionEnter (Collision other) {
-		string name = other.gameObject.name;
-		// print("THIS " + name);
-		if(name == "Base")
-		{
-			// HitBase();
-			Destroy(gameObject);
-		}
-		if(name == "Bullet")
-		{ 
-			//var damage = other.gameObject.GetComponent<Bullet>().whatever;
-			//Health -= damage;
-            if (Health<=0)
+    Health = 25f;
+    Damage = 5f;
+    Speed = 0.5f;
+  }
+
+   internal void OnCollisionEnter (Collision other) {
+    if(other.gameObject.GetComponent<Base>())
+    {
+      // HitBase();
+      Destroy(gameObject);
+    }
+    if(other.gameObject.GetComponentInChildren<Bullet>())
+    {
+            if (other.gameObject.GetComponentInChildren<Bullet>().flag)
             {
-                Destroy(gameObject);
+                float damage = other.gameObject.GetComponentInChildren<Bullet>().damage;
+                Health -= damage;
+                Debug.Log(Health);
+                if (Health <= 0)
+                {
+                    Destroy(gameObject);
+                }
+                other.gameObject.GetComponentInChildren<Bullet>().flag = false;
             }
-		}
-	}
-	
-	void FixedUpdate() {
-		float step = Speed * Time.deltaTime;
-		transform.position = Vector3.MoveTowards(transform.position, Target.position, step);
+
+    }
+  }
+
+  void FixedUpdate() {
+    float step = Speed * Time.deltaTime;
+    transform.position = Vector3.MoveTowards(transform.position, Target.position, step);
         //transform.position = Vector3.MoveTowards(transform.position, new Vector3(4,0,0), step);
     }
 }
--- Assets/Tower.cs
+++ Assets/Tower.cs
@@ -5,31 +5,57 @@ public class Tower : MonoBehaviour {
     // Use this for initialization
     void Start () {
         this.transform.position = new Vector3(1 , 1, -0.7f);
-	}
-	void TurnToEnemy()
-    {
-        
     }
-	// Update is called once per frame
-	void Update() {
-        RaycastHit[] hitinfo;
+    void TurnToEnemy()
+    {
+
+    }
+    // Update is called once per frame
+    void Update() {
+        /*RaycastHit[] hitinfo;
         var layermask = 1 << 8;
+        if (Physics.SphereCast(transform.position, 0.5f, transform.forward, out hitinfo, 1.5f, layermask))
+        {
+            Vector3 relativePos = hitinfo.transform.position - transform.position;
+            Quaternion rotation = Quaternion.LookRotation(relativePos, transform.up);
+            transform.rotation = rotation;
+            GetComponentInChildren<Gun>().FireAt(hitinfo.transform);
+        }
         hitinfo = Physics.SphereCastAll(transform.position, 0.5f, transform.forward, 1.5f, layermask);
         if (hitinfo.Length != 0)
         {
             int nearest = 0;
             for (int i = 0; i < hitinfo.Length; i++)
             {
-                if (hitinfo[i].distance < hitinfo[nearest].distance)
+                if (hitinfo[i].distance <= hitinfo[nearest].distance)
                 {
                     nearest = i;
                 }
             }
             Vector3 relativePos = hitinfo[nearest].transform.position - transform.position;
-            Quaternion rotation = Quaternion.LookRotation(relativePos);
-            Quaternion rotationnew = Quaternion.Euler(rotation.x, rotation.y, rotation.z);
+            Quaternion rotation = Quaternion.LookRotation(relativePos, transform.up);
             transform.rotation = rotation;
             GetComponentInChildren<Gun>().FireAt(hitinfo[nearest].transform);
+        }*/
+        var layermask = 1 << 8;
+        Collider[] enemyInRange = Physics.OverlapSphere(transform.position, 2f, layermask);
+        int nearest = 0;
+        if (enemyInRange.Length > 0)
+        {
+            for (int i = 0; i < enemyInRange.Length; i++)
+            {
+                var distance = Vector3.Distance(enemyInRange[i].transform.position, transform.position);
+                var nearestDistance = Vector3.Distance(enemyInRange[nearest].transform.position, transform.position);
+                if (distance < nearestDistance)
+                {
+                    nearest = i;
+                }
+            }
+            Vector3 relativePos = enemyInRange[nearest].transform.position - transform.position;
+            Quaternion rotation = Quaternion.LookRotation(relativePos, transform.up);
+            transform.rotation = rotation;
+            GetComponentInChildren<Gun>().FireAt(enemyInRange[nearest].transform);
         }
-	}
+
+    }
 }

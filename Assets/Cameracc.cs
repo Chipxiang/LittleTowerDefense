f9d186b98bd78a4d285047775633eecebc7ac5c7
fbybeetle <461971750@qq.com>
2017-11-05 19:07:29 -0600

    camera

---
+++ Assets/Cameracc.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+
+public class Cameracc : MonoBehaviour
+{
+
+    Transform target;
+    public float horiz;
+    public float verti;
+
+    public float panSpeed = 0.03f;
+    public float panBorderThickness = 0.1f;
+
+    public float scrollSpeed = 0.1f;
+    public float minY = 15f;
+    public float maxY = 35f;
+
+    private void Start()
+    {
+        Debug.Log(Screen.height);
+        Debug.Log(Screen.width);
+        Debug.Log(Screen.resolutions);
+        //Quaternion rotation = Quaternion.Euler(-2.222f, 0, 0);
+        //transform.SetPositionAndRotation(new Vector3(3.78f, 2.47f, -87.14f), rotation);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            
+            transform.Translate(Vector3.up*0.01f * panSpeed * Time.deltaTime, Space.World);
+            Debug.Log(transform.position);
+        }
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+        {
+            transform.Translate(Vector3.down * 0.01f* panSpeed * Time.deltaTime, Space.World);
+            Debug.Log(transform.position);
+        }
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            transform.Translate(Vector3.right * 0.01f * panSpeed * Time.deltaTime, Space.World);
+            Debug.Log(transform.position);
+        }
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+        {
+            transform.Translate(Vector3.left * 0.01f * panSpeed * Time.deltaTime, Space.World);
+            Debug.Log(transform.position);
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+
+            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+            Debug.Log(transform.position);
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+
+            transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+            Debug.Log(transform.position);
+        }
+
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        Vector3 pos = transform.position;
+
+        //pos.x -= scroll * 0.1f * scrollSpeed * Time.deltaTime;
+        //pos.x = Mathf.Clamp(pos.x, minY, maxY);
+
+
+
+        transform.position  = pos;
+
+    }
+}

195faa2419d5813031bfa7c84557ab2a19abfdac
fbybeetle <461971750@qq.com>
2017-11-11 22:58:53 -0600

    change the location of the grid

--- Assets/Cell.cs
+++ Assets/Cell.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cell : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+namespace Assets.Code.Menus
+{
+    public class Cell : MonoBehaviour
+    {
+        public static bool built_flag;
+        public static bool path_flag;
+
+        // Use this for initialization
+        void Start()
+        {
+            built_flag = true;
+            path_flag = true;
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
 }
---
+++ Assets/Mainmenu.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Code.Menus
+{
+    public partial class MenuManager
+    {
+        private class Mainmenu : Menu
+        {
+            private readonly Object _menu;
+            public Mainmenu()
+            {
+                _menu = Resources.Load("Menus/Mainmenu");
+                Go = GameObject.Instantiate(_menu) as GameObject;
+                // Debug.Log(Go.name);
+                //Go.AddComponent<Button>();
+                InitializeButtons();
+                // TODO fill me in
+            }
+
+            /// <summary>
+            /// Add listeners to the MainMenu buttons
+            /// </summary>
+            private void InitializeButtons()
+            {
+                var newb = Go.GetComponentsInChildren<Button>();
+                foreach (Button but in newb)
+                {
+                    if (but.name == "build1") { }
+                    //build a tower
+
+                    if (but.name == "build2") { }
+                        
+                }
+                Go.transform.SetParent(Canvas, false);//Canvas come from the UIma.cs.
+
+                //newb.onClick.AddListener(() => Game.Ctx.StartGame());
+                // TODO fill me in
+            }
+
+
+
+
+
+        }
+    }
+}

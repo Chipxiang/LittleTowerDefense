e7a4dd5ab5f0c05c44ad96f70cd0f51a2f32f015
Chipxiang <jianxiangli1996@gmail.com>
2017-12-01 15:13:33 -0600

    Modified ShockTower

--- Assets/ShockTower.cs
+++ Assets/ShockTower.cs
@@ -11,6 +11,8 @@ public class ShockTower : MonoBehaviour {
     void Start()
     {
         enemyInRange = new List<GameObject>();
+        _mat = Resources.Load("ShockTowerMaterial", typeof(Material)) as Material;
+
     }
     IEnumerator DrawLine(Transform v1, Transform v2, Transform v3)
     {
@@ -28,26 +30,43 @@ public class ShockTower : MonoBehaviour {
     // Update is called once per frame
     void FixedUpdate () {
         if(enemyInRange.Count != 0)
-        { 
-            if (Time.time - _lastHit > 2)
+        {
+            if (enemyInRange[0] == null)
+                enemyInRange.RemoveAt(0);
+            else if (Time.time - _lastHit > 2)
             {
                 _lastHit = Time.time;
+                Transform v1, v2, v3;
                 int nearest = 0;
                 for (int i =0;i < enemyInRange.Count; i++)
                 {
-                    var distance = Vector3.Distance(enemyInRange[i].transform.position, transform.position);
-                    var nearestDistance = Vector3.Distance(enemyInRange[nearest].transform.position, transform.position);
-                    if (distance < nearestDistance)
+                    if (enemyInRange[i] == null)
                     {
-                        nearest = i;
+                        enemyInRange.RemoveAt(i);
+                        i--;
+                    } 
+                    else
+                    {
+                        var distance = Vector3.Distance(enemyInRange[i].transform.position, transform.position);
+                        var nearestDistance = Vector3.Distance(enemyInRange[nearest].transform.position, transform.position);
+                        if (distance < nearestDistance)
+                        {
+                            nearest = i;
+                        }
                     }
                 }
-
-                Collider[] nearByEnemy = Physics.OverlapSphere(enemyInRange[nearest].transform.position, 1.5f, layermask);
-                int nearest1 = 0;
-                if (nearByEnemy.Length != 0)
+                v1 = enemyInRange[0].transform;
+                v2 = enemyInRange[0].transform;
+                v3 = enemyInRange[0].transform;
+                if (enemyInRange.Count>1)
+                    v2 = enemyInRange[1].transform;
+                if(enemyInRange.Count>2)
+                    v3 = enemyInRange[2].transform;
+                /*Collider[] nearByEnemy = Physics.OverlapSphere(enemyInRange[nearest].transform.position, 1.5f, layermask);
+                int nearest1 = 1;
+                if (nearByEnemy.Length >= 2)
                 {
-                    for (int i = 0; i < nearByEnemy.Length; i++)
+                    for (int i = 1; i < nearByEnemy.Length; i++)
                     {
                         var distance = Vector3.Distance(nearByEnemy[i].transform.position, enemyInRange[nearest].transform.position);
                         var nearestDistance = Vector3.Distance(nearByEnemy[nearest1].transform.position, enemyInRange[nearest].transform.position);
@@ -56,11 +75,12 @@ public class ShockTower : MonoBehaviour {
                             nearest1 = i;
                         }
                     }
+                    v2 = nearByEnemy[nearest1].transform;
                     int nearest2 = 0;
                     Collider[] nearByEnemy2 = Physics.OverlapSphere(nearByEnemy[nearest1].transform.position, 1.5f, layermask);
-                    if (nearByEnemy2.Length != 0)
+                    if (nearByEnemy2.Length >= 1)
                     {
-                        for (int i = 0; i < nearByEnemy2.Length; i++)
+                        for (int i = 1; i < nearByEnemy2.Length; i++)
                         {
                             var distance = Vector3.Distance(nearByEnemy2[i].transform.position, nearByEnemy[nearest1].transform.position);
                             var nearestDistance = Vector3.Distance(nearByEnemy2[nearest2].transform.position, nearByEnemy[nearest1].transform.position);
@@ -69,8 +89,11 @@ public class ShockTower : MonoBehaviour {
                                 nearest2 = i;
                             }
                         }
+                        v3 = nearByEnemy[nearest1].transform;
                     }
-                }
+                }*/
+                Debug.Log("Positions: "+v1.position+ v2.position +v3.position);
+                StartCoroutine(DrawLine(v1, v2, v3));
             }
         }
 		
